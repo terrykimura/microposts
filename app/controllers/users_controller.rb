d86ac7f31bef1b18a30a412458c0f2b484d4cfeb
terrykimura <terry.kimura@gmail.com>
2015-07-26 11:59:08 +0000

    add posting microposts

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+  
+  def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts
+  end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+    @user = User.edit
+  end
+  
+  def update
+    @user = User.update
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
