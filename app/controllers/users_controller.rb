9080a1a157bd73e162085c1e78b859b731be9ee0
terrykimura <terry.kimura@gmail.com>
2015-08-06 10:22:54 +0000

    Added bio as per user-profile instructions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
   private
   
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :location)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :location, :bio)
   end
   
   def set_user
